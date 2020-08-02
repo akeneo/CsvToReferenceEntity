e02d234aba973c97f7ae60d5a499b2d84a53c3f1
Sylvain Rayé <sly@diglin.com>
2020-08-02 17:25:18 +0200

    Add strict type

--- src/Processor/Converter/MediaConverter.php
+++ src/Processor/Converter/MediaConverter.php
@@ -8,12 +8,18 @@
  * @copyright   Copyright (c) Diglin (https://www.diglin.com)
  */
 
+declare(strict_types=1);
+
 namespace App\Processor\Converter;
 
 class MediaConverter implements DataConverterInterface
 {
     /**
      * Does this data converter support the given $attribute
+     *
+     * @param array $attribute
+     *
+     * @return bool
      */
     public function support(array $attribute): bool
     {
@@ -22,8 +28,13 @@ class MediaConverter implements DataConverterInterface
 
     /**
      * Convert the given $data for the given $attribute to the correct format expected by the Akeneo API
+     *
+     * @param array $attribute
+     * @param string $data
+     *
+     * @return string|null
      */
-    public function convert(array $attribute, string $data)
+    public function convert(array $attribute, string $data): ?string
     {
         if (empty($data)) {
             return null;
